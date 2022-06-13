338cebad13849d6dccbe94d73faa7639f97e9d96
yosef1sh <yos17sh@hotmail.com>
2022-06-13 20:31:14 +0300

    change

---
+++ server/models/Cost.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const CostSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    sum: {
+      type: Number,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cost", CostSchema);
