f60b383180c3330e4fd85a4b910403841a118daf
yosef1sh <yos17sh@hotmail.com>
2022-06-19 21:03:17 +0300

    Update Cost.jsx

--- client/src/components/cost/Cost.jsx
+++ client/src/components/cost/Cost.jsx
@@ -12,9 +12,9 @@ export default function Cost({ cost }) {
       <br></br>
       {new Date(cost.createdAt).toDateString()}
     </span>
-    <p>{cost.username}</p><br></br>
-    <p>{cost.category}</p><br></br>
-    <p>{cost.sum}</p><br></br>
+    <p>user: {cost.username}</p><br></br>
+    <p>category: {cost.category}</p><br></br>
+    <p>sum: {cost.sum}</p><br></br>
     <Link to={`/cost/${cost._id}`} className="link">
     צפה בפעולה
     </Link>
